919afa68e8965cda15dacf77308ed93db779d5fa
will <cla.lixo@hotmail.com>
2019-06-01 07:15:58 -0300

    versao 0.1

---
+++ myapp/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from .models import Music
+from .serializers import MusicSerializer
+
+from django_filters import rest_framework as filters
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_condition import Or
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope, OAuth2Authentication
+from rest_framework.authentication import SessionAuthentication
+
+class MusicList(generics.ListCreateAPIView):
+
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+    authentication_classes = [OAuth2Authentication, SessionAuthentication]
+    permission_classes = [Or(IsAdminUser, TokenHasReadWriteScope)]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = '__all__'
+
+
